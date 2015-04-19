f45e1263bb5ea6e016b5966e803b25b434576519
Ray Donnelly <mingw.android@gmail.com>
2015-04-19 08:36:26 +0100

    genagiledll: Fixes for __MINGW_EXPORTED_IF_AGILE
    Can't be defined as nothing, so use "" instead.

--- mingw-w64-crt/misc/invalid_parameter_handler.c
+++ mingw-w64-crt/misc/invalid_parameter_handler.c
@@ -8,7 +8,7 @@ __MINGW_EXPORTED_IF_AGILE _invalid_parameter_handler __cdecl mingw_set_invalid_p
     return InterlockedExchangePointer((void**)&handler, new_handler);
 }
 
-#ifndef(_BUILDING_AGILE_DLLIMP)
+#ifndef _BUILDING_AGILE_DLLIMP
 _invalid_parameter_handler (__cdecl *__MINGW_IMP_SYMBOL(_set_invalid_parameter_handler))(_invalid_parameter_handler) =
     mingw_set_invalid_parameter_handler;
 #endif
@@ -18,7 +18,7 @@ __MINGW_EXPORTED_IF_AGILE _invalid_parameter_handler __cdecl mingw_get_invalid_p
     return handler;
 }
 
-#ifndef(_BUILDING_AGILE_DLLIMP)
+#ifndef _BUILDING_AGILE_DLLIMP
 _invalid_parameter_handler (__cdecl *__MINGW_IMP_SYMBOL(_get_invalid_parameter_handler))(void) = mingw_get_invalid_parameter_handler;
 
 _invalid_parameter_handler __cdecl _get_invalid_parameter_handler(void) __attribute__ ((alias ("mingw_get_invalid_parameter_handler")));
