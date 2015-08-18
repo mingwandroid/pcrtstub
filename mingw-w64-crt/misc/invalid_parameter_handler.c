556a4a5db7a60ca2b0f2d97d8008c707a3e3e9ff
Ray Donnelly <mingw.android@gmail.com>
2015-08-18 22:21:37 +0100

    misc/invalid_parameter_handler.c: Hack for proof-of-concept

--- mingw-w64-crt/misc/invalid_parameter_handler.c
+++ mingw-w64-crt/misc/invalid_parameter_handler.c
@@ -8,19 +8,19 @@ __MINGW_EXPORTED_IF_AGILE _invalid_parameter_handler __cdecl mingw_set_invalid_p
     return InterlockedExchangePointer((void**)&handler, new_handler);
 }
 
-#ifndef _BUILDING_AGILE_DLLIMP
+/*#ifndef _BUILDING_AGILE_DLLIMP*/
 _invalid_parameter_handler (__cdecl *__MINGW_IMP_SYMBOL(_set_invalid_parameter_handler))(_invalid_parameter_handler) =
     mingw_set_invalid_parameter_handler;
-#endif
+/*#endif*/
 
 __MINGW_EXPORTED_IF_AGILE _invalid_parameter_handler __cdecl mingw_get_invalid_parameter_handler(void)
 {
     return handler;
 }
 
-#ifndef _BUILDING_AGILE_DLLIMP
+/*#ifndef _BUILDING_AGILE_DLLIMP*/
 _invalid_parameter_handler (__cdecl *__MINGW_IMP_SYMBOL(_get_invalid_parameter_handler))(void) = mingw_get_invalid_parameter_handler;
 
 _invalid_parameter_handler __cdecl _get_invalid_parameter_handler(void) __attribute__ ((alias ("mingw_get_invalid_parameter_handler")));
 _invalid_parameter_handler __cdecl _set_invalid_parameter_handler(_invalid_parameter_handler new_handler) __attribute__ ((alias ("mingw_set_invalid_parameter_handler")));
-#endif
+/*#endif*/
